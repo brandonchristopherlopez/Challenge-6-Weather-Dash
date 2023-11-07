75a277e67d972faff557b73346b988ac3b37e48c
Brandon_Lopez <brandonchristopherlopez@gmail.com>
2023-11-06 16:51:26 -0800

    started js page

---
+++ index.js
@@ -0,0 +1,11 @@
+var searchHistory [];
+var weatherApiRootURL = 'https://api.openweathermap.org';
+var weatherApiKey = 'xyz'
+
+
+var searchForm = document.querySelector('#search-form');
+var searchInput = document.querySelector('#search-input');
+var todayContainer = document.querySelector('#today');
+var forecastContainer = document.querySelector('#forecast');
+var searchHistoryContainer = document.querySelector('#history');
+
