4633cd4582ac980366a872cd5fd16c67168d2110
Brandon_Lopez <brandonchristopherlopez@gmail.com>
2023-11-06 17:25:40 -0800

    Finished touches on js

--- index.js
+++ index.js
@@ -9,31 +9,25 @@ var todayContainer = document.querySelector('#today');
 var forecastContainer = document.querySelector('#forecast');
 var searchHistoryContainer = document.querySelector('#history');
 
-// timezone pluigin? 
 dayjs.extend(window.dayjs_plugin_utc);
 dayjs.extend(window.dayjs_plugin_timezone);
 
-// Function to display the search history list.
 function renderSearchHistory() {
   searchHistoryContainer.innerHTML = '';
 
-  // Start at end of history array and count down to show the most recent at the top.
   for (var i = searchHistory.length - 1; i >= 0; i--) {
     var btn = document.createElement('button');
     btn.setAttribute('type', 'button');
     btn.setAttribute('aria-controls', 'today forecast');
     btn.classList.add('history-btn', 'btn-history');
 
-    // `data-search` allows access to city name when click handler is invoked
     btn.setAttribute('data-search', searchHistory[i]);
     btn.textContent = searchHistory[i];
     searchHistoryContainer.append(btn);
   }
 }
 
-// Function to update history in local storage then updates displayed history.
 function appendToHistory(search) {
-    // If there is no search term return the function
     if (searchHistory.indexOf(search) !== -1) {
       return;
     }
@@ -43,7 +37,6 @@ function appendToHistory(search) {
     renderSearchHistory();
   }
   
-  // Function to get search history from local storage
   function initSearchHistory() {
     var storedHistory = localStorage.getItem('search-history');
     if (storedHistory) {
@@ -52,10 +45,10 @@ function appendToHistory(search) {
     renderSearchHistory();
   }
   
-  // Function to display the current weather data fetched from OpenWeather api.
   function renderCurrentWeather(city, weather) {
     var date = dayjs().format('M/D/YYYY');
-    // Store response data from our fetch request in variables
+
+
     var tempF = weather.main.temp;
     var windMph = weather.wind.speed;
     var humidity = weather.main.humidity;
@@ -92,18 +85,14 @@ function appendToHistory(search) {
     todayContainer.innerHTML = '';
     todayContainer.append(card);
   }
-  $("div").
-  // Function to display a forecast card given an object from open weather api
-  // daily forecast.
   function renderForecastCard(forecast) {
-    // variables for data from api
+
     var iconUrl = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
     var iconDescription = forecast.weather[0].description;
     var tempF = forecast.main.temp;
     var humidity = forecast.main.humidity;
     var windMph = forecast.wind.speed;
   
-    // Create elements for a card
     var col = document.createElement('div');
     var card = document.createElement('div');
     var cardBody = document.createElement('div');
@@ -126,7 +115,6 @@ function appendToHistory(search) {
     windEl.setAttribute('class', 'card-text');
     humidityEl.setAttribute('class', 'card-text');
   
-    // Add content to elements
     cardTitle.textContent = dayjs(forecast.dt_txt).format('M/D/YYYY');
     weatherIcon.setAttribute('src', iconUrl);
     weatherIcon.setAttribute('alt', iconDescription);
@@ -136,10 +124,7 @@ function appendToHistory(search) {
   
     forecastContainer.append(col);
   }
-  
-  // Function to display 5 day forecast.
-  function renderForecast(dailyForecast) {
-    // Create unix timestamps for start and end of 5 day forecast
+    function renderForecast(dailyForecast) {
     var startDt = dayjs().add(1, 'day').startOf('day').unix();
     var endDt = dayjs().add(6, 'day').startOf('day').unix();
   
@@ -155,14 +140,81 @@ function appendToHistory(search) {
   
     for (var i = 0; i < dailyForecast.length; i++) {
   
-      // First filters through all of the data and returns only data that falls between one day after the current data and up to 5 days later.
       if (dailyForecast[i].dt >= startDt && dailyForecast[i].dt < endDt) {
   
-        // Then filters through the data and returns only data captured at noon for each day.
         if (dailyForecast[i].dt_txt.slice(11, 13) == "12") {
           renderForecastCard(dailyForecast[i]);
         }
       }
     }
   }
-  git 
+
+  function renderItems(city, data) {
+    renderCurrentWeather(city, data.list[0], data.city.timezone);
+    renderForecast(data.list);
+  }
+  
+  function fetchWeather(location) {
+    var { lat } = location;
+    var { lon } = location;
+    var city = location.name;
+  
+    var apiUrl = `${weatherApiRootUrl}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${weatherApiKey}`;
+  
+    fetch(apiUrl)
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (data) {
+        renderItems(city, data);
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  }
+  
+  function fetchCoords(search) {
+    var apiUrl = `${weatherApiRootUrl}/geo/1.0/direct?q=${search}&limit=5&appid=${weatherApiKey}`;
+  
+    fetch(apiUrl)
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (data) {
+        if (!data[0]) {
+          alert('Location not found');
+        } else {
+          appendToHistory(search);
+          fetchWeather(data[0]);
+        }
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  }
+  
+  function handleSearchFormSubmit(e) {
+    if (!searchInput.value) {
+      return;
+    }
+  
+    e.preventDefault();
+    var search = searchInput.value.trim();
+    fetchCoords(search);
+    searchInput.value = '';
+  }
+  
+  function handleSearchHistoryClick(e) {
+    if (!e.target.matches('.btn-history')) {
+      return;
+    }
+  
+    var btn = e.target;
+    var search = btn.getAttribute('data-search');
+    fetchCoords(search);
+  }
+  
+  initSearchHistory();
+  searchForm.addEventListener('submit', handleSearchFormSubmit);
+  searchHistoryContainer.addEventListener('click', handleSearchHistoryClick);
+  
